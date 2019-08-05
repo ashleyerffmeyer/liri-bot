ad5d47bc12ab55544a7fa14fa0c2eceeffeea16a
ashleyerffmeyer <erffmea@gmail.com>
2019-08-05 17:06:06 -0500

    initial code in liri.js file

--- liri.js
+++ liri.js
@@ -1,16 +1,28 @@
 require("dotenv").config();
 
+var axios = require("axios");
+
 var keys = require("./keys.js");
 
 var spotify = new Spotify(keys.spotify);
 
 // node liri.js concert-this <artist/band name here>
-var artist = 
+var artist = process.argv[2];
 
 var bandsURL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
 
+axios
+    .get(bandsURL)
+    .then(function (response) {
+
+        console.log(response.data);
+    })
+    .catch(function (error) {
+        console.log("error");
+    });
+
 // node liri.js spotify-this-song <song name here>
 
-// node liri.js movie-this <movie name here>
+// node liri.js movie-this '<movie name here>'
 
 // node liri.js do-what-it-says
