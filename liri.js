92a1c4a951f7be40f9866ce6873c7972c1759959
ashleyerffmeyer <erffmea@gmail.com>
2019-08-06 13:15:22 -0500

    adding intial functionality for movie-this and spotify-this-song commands

--- liri.js
+++ liri.js
@@ -1,15 +1,16 @@
 require("dotenv").config();
 
-var axios = require("axios");
-
 var keys = require("./keys.js");
 
-var spotify = new Spotify(keys.spotify);
+var axios = require("axios");
+
+var Spotify = require('node-spotify-api');
+
+
 
 // node liri.js concert-this <artist/band name here>
-var artist = process.argv[2];
 
-var bandsURL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
+/*var bandsURL = "https://rest.bandsintown.com/artists/" + name + "/events?app_id=codingbootcamp";
 
 axios
     .get(bandsURL)
@@ -19,10 +20,122 @@ axios
     })
     .catch(function (error) {
         console.log("error");
-    });
+    }); **/
 
 // node liri.js spotify-this-song <song name here>
 
+var getArtistName = function (artist) {
+    return artist.name;
+}
+
+var spotifyThisSong = function (songName) {
+
+    var spotify = new Spotify(keys.spotify);
+
+    spotify.search({ type: 'track', query: songName }, function (err, data) {
+        if (err) {
+            return console.log('Error occurred: ' + err);
+        }
+
+        console.log(data);
+
+        var songs = data.tracks.items;
+        console.log(songs);
+        for (var i = 0; i < songs.length; i++) {
+            console.log(i);
+            console.log("Artist(s): " + songs[i].artists.map(getArtistName));
+            console.log("Song Name: " + songs[i].name);
+            console.log("Preview Link: " + songs[i].href);
+            console.log("Album Name: " + songs[i].album.name);
+            console.log("----------------------------------------------------");
+
+        }
+    });
+};
+
 // node liri.js movie-this '<movie name here>'
 
+var movieThis = function (movieName) {
+
+    // Store all of the arguments in an array
+    //var nodeArgs = process.argv;
+
+    // Create an empty variable for holding the movie name
+    //var movieName = "";
+
+    // Loop through all the words in the node argument
+    // And do a little for-loop magic to handle the inclusion of "+"s
+
+    /** for (var i = 2; i < nodeArgs.length; i++) {
+ 
+         if (i > 2 && i < nodeArgs.length) {
+             movieName = movieName + "+" + nodeArgs[i];
+         } else {
+             movieName += nodeArgs[i];
+ 
+         }
+ } */
+
+    // Then run a request with axios to the OMDB API with the movie specified
+    var omdbQueryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
+
+    // This line is just to help us debug against the actual URL.
+    console.log(omdbQueryUrl);
+
+    axios.get(omdbQueryUrl).then(
+        function (response) {
+            console.log("Movie Title: " + response.data.Title);
+            console.log("Release Year: " + response.data.Year);
+            console.log("IMDB Rating: " + response.data.imdbRating);
+            console.log("Rotten Tomatoes Rating: " + response.data.Metascore);
+            console.log("Production Country: " + response.data.Country);
+            console.log("Movie Language: " + response.data.Language);
+            console.log("Movie Plot: " + response.data.Plot);
+            console.log("Featured Actors: " + response.data.Actors);
+        })
+        .catch(function (error) {
+            if (error.response) {
+                // The request was made and the server responded with a status code
+                // that falls out of the range of 2xx
+                console.log("---------------Data---------------");
+                console.log(error.response.data);
+                console.log("---------------Status---------------");
+                console.log(error.response.status);
+                console.log("---------------Status---------------");
+                console.log(error.response.headers);
+            } else if (error.request) {
+                // The request was made but no response was received
+                // `error.request` is an object that comes back with details pertaining to the error that occurred.
+                console.log(error.request);
+            } else {
+                // Something happened in setting up the request that triggered an Error
+                console.log("Error", error.message);
+            }
+            console.log(error.config);
+        });
+};
+
+
 // node liri.js do-what-it-says
+
+// Functions
+
+var pick = function (caseData, functionData) {
+    switch (caseData) {
+        case 'movie-this':
+            movieThis(functionData);
+            break;
+
+        case 'spotify-this-song':
+            spotifyThisSong(functionData);
+            break;
+
+        default:
+            console.log('LIRI does not know this');
+    }
+}
+var runThis = function (argOne, argTwo) {
+    pick(argOne, argTwo);
+}
+
+runThis(process.argv[2], process.argv[3]);
